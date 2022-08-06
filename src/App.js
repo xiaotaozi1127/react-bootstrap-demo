69da9c96192d089e987f2a651abd8a75d131ea03
taohui <htao@thoughtworks.com>
2022-08-06 17:22:38 +0800

    simple demo for react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Form from "react-bootstrap/Form";
+
+import "./App.css";
+
+const handleChange = (event) => {
+  let targetValue = event.target.value;
+  //when i type 3.5, it will be triggered twice, first time value is 3, second time value is 3.5.
+  console.log("target value=" + targetValue);
+};
+
+const App = () => (
+    <Form.Control type="number" defaultValue={0} onChange={handleChange} />
+);
 
 export default App;
